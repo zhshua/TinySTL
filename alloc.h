1009ad06c94bb0316ba6f9b440b13c497b2019f5
zhshu <869528598@qq.com>
2020-10-29 22:50:53 +0800

    update

--- alloc.h
+++ alloc.h
@@ -1,7 +1,7 @@
 #ifndef _ALLOC_H_
 #define _ALLOC_H_
 
-#include <cstddef>
+#include <cstdlib>
 
 namespace TinySTL{
     class Alloc{
@@ -35,7 +35,7 @@ namespace TinySTL{
         static void *refill(size_t n);
     public:
         static void *allocate(size_t bytes);
-        static void *dellocate(void *ptr, size_t bytes);
+        static void dellocate(void *ptr, size_t bytes);
         static void *reallocate(void *ptr, size_t old_sz, size_t new_sz);
     };
 }
